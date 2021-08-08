56a3581bbf57d3325cc127022ea7a01a16a9e8d5
Allison Brito <adbrito@espol.edu.ec>
2021-08-07 20:01:16 -0500

    cambios

--- prueba.py
+++ prueba.py
@@ -21,193 +21,77 @@ import emoji
 def load_dict_smileys():
     
     return {
-        ":‑)":"smiley",
-        ":-]":"smiley",
-        ":-3":"smiley",
-        ":->":"smiley",
-        "8-)":"smiley",
-        ":-}":"smiley",
-        ":)":"smiley",
-        ":]":"smiley",
-        ":3":"smiley",
-        ":>":"smiley",
-        "8)":"smiley",
-        ":}":"smiley",
-        ":o)":"smiley",
-        ":c)":"smiley",
-        ":^)":"smiley",
-        "=]":"smiley",
-        "=)":"smiley",
-        ":-))":"smiley",
-        ":‑D":"smiley",
-        "8‑D":"smiley",
-        "x‑D":"smiley",
-        "X‑D":"smiley",
-        ":D":"smiley",
-        "8D":"smiley",
-        "xD":"smiley",
-        "XD":"smiley",
-        ":‑(":"sad",
-        ":‑c":"sad",
-        ":‑<":"sad",
-        ":‑[":"sad",
-        ":(":"sad",
-        ":c":"sad",
-        ":<":"sad",
-        ":[":"sad",
-        ":-||":"sad",
-        ">:[":"sad",
-        ":{":"sad",
-        ":@":"sad",
-        ">:(":"sad",
-        ":'‑(":"sad",
-        ":'(":"sad",
-        ":‑P":"playful",
-        "X‑P":"playful",
-        "x‑p":"playful",
-        ":‑p":"playful",
-        ":‑Þ":"playful",
-        ":‑þ":"playful",
-        ":‑b":"playful",
-        ":P":"playful",
-        "XP":"playful",
-        "xp":"playful",
-        ":p":"playful",
-        ":Þ":"playful",
-        ":þ":"playful",
-        ":b":"playful",
-        "<3":"love"
+        ":‑)":"sonriente",
+        ":-]":"sonriente",
+        ":-3":"sonriente",
+        ":->":"sonriente",
+        "8-)":"sonriente",
+        ":-}":"sonriente",
+        ":)":"sonriente",
+        ":]":"sonriente",
+        ":3":"sonriente",
+        ":>":"sonriente",
+        "8)":"sonriente",
+        ":}":"sonriente",
+        ":o)":"sonriente",
+        ":c)":"sonriente",
+        ":^)":"sonriente",
+        "=]":"sonriente",
+        "=)":"sonriente",
+        ":-))":"sonriente",
+        ":‑D":"sonriente",
+        "8‑D":"sonriente",
+        "x‑D":"sonriente",
+        "X‑D":"sonriente",
+        ":D":"sonriente",
+        "8D":"sonriente",
+        "xD":"sonriente",
+        "XD":"sonriente",
+        "Xd":"sonriente",
+        ":‑(":"triste",
+        ":‑c":"triste",
+        ":‑<":"triste",
+        ":‑[":"triste",
+        ":(":"triste",
+        ":c":"triste",
+        ":<":"triste",
+        ":[":"triste",
+        ":-||":"triste",
+        ">:[":"triste",
+        ":{":"triste",
+        ":@":"triste",
+        ">:(":"triste",
+        ":'‑(":"triste",
+        ":'(":"triste",
+        ":‑P":"jugueton",
+        "X‑P":"jugueton",
+        "x‑p":"jugueton",
+        ":‑p":"jugueton",
+        ":‑Þ":"jugueton",
+        ":‑þ":"jugueton",
+        ":‑b":"jugueton",
+        ":P":"jugueton",
+        "XP":"jugueton",
+        "xp":"jugueton",
+        ":p":"jugueton",
+        ":Þ":"jugueton",
+        ":þ":"jugueton",
+        ":b":"jugueton",
+        "<3":"amor",
+        ":*":"amoroso"
         }
 
 # self defined contractions
 def load_dict_contractions():
     
     return {
-        "ain't":"is not",
-        "amn't":"am not",
-        "aren't":"are not",
-        "can't":"cannot",
-        "'cause":"because",
-        "couldn't":"could not",
-        "couldn't've":"could not have",
-        "could've":"could have",
-        "daren't":"dare not",
-        "daresn't":"dare not",
-        "dasn't":"dare not",
-        "didn't":"did not",
-        "doesn't":"does not",
-        "don't":"do not",
-        "e'er":"ever",
-        "em":"them",
-        "everyone's":"everyone is",
-        "finna":"fixing to",
-        "gimme":"give me",
-        "gonna":"going to",
-        "gon't":"go not",
-        "gotta":"got to",
-        "hadn't":"had not",
-        "hasn't":"has not",
-        "haven't":"have not",
-        "he'd":"he would",
-        "he'll":"he will",
-        "he's":"he is",
-        "he've":"he have",
-        "how'd":"how would",
-        "how'll":"how will",
-        "how're":"how are",
-        "how's":"how is",
-        "I'd":"I would",
-        "I'll":"I will",
-        "I'm":"I am",
-        "I'm'a":"I am about to",
-        "I'm'o":"I am going to",
-        "isn't":"is not",
-        "it'd":"it would",
-        "it'll":"it will",
-        "it's":"it is",
-        "I've":"I have",
-        "kinda":"kind of",
-        "let's":"let us",
-        "mayn't":"may not",
-        "may've":"may have",
-        "mightn't":"might not",
-        "might've":"might have",
-        "mustn't":"must not",
-        "mustn't've":"must not have",
-        "must've":"must have",
-        "needn't":"need not",
-        "ne'er":"never",
-        "o'":"of",
-        "o'er":"over",
-        "ol'":"old",
-        "oughtn't":"ought not",
-        "shalln't":"shall not",
-        "shan't":"shall not",
-        "she'd":"she would",
-        "she'll":"she will",
-        "she's":"she is",
-        "shouldn't":"should not",
-        "shouldn't've":"should not have",
-        "should've":"should have",
-        "somebody's":"somebody is",
-        "someone's":"someone is",
-        "something's":"something is",
-        "that'd":"that would",
-        "that'll":"that will",
-        "that're":"that are",
-        "that's":"that is",
-        "there'd":"there would",
-        "there'll":"there will",
-        "there're":"there are",
-        "there's":"there is",
-        "these're":"these are",
-        "they'd":"they would",
-        "they'll":"they will",
-        "they're":"they are",
-        "they've":"they have",
-        "this's":"this is",
-        "those're":"those are",
-        "'tis":"it is",
-        "'twas":"it was",
-        "wanna":"want to",
-        "wasn't":"was not",
-        "we'd":"we would",
-        "we'd've":"we would have",
-        "we'll":"we will",
-        "we're":"we are",
-        "weren't":"were not",
-        "we've":"we have",
-        "what'd":"what did",
-        "what'll":"what will",
-        "what're":"what are",
-        "what's":"what is",
-        "what've":"what have",
-        "when's":"when is",
-        "where'd":"where did",
-        "where're":"where are",
-        "where's":"where is",
-        "where've":"where have",
-        "which's":"which is",
-        "who'd":"who would",
-        "who'd've":"who would have",
-        "who'll":"who will",
-        "who're":"who are",
-        "who's":"who is",
-        "who've":"who have",
-        "why'd":"why did",
-        "why're":"why are",
-        "why's":"why is",
-        "won't":"will not",
-        "wouldn't":"would not",
-        "would've":"would have",
-        "y'all":"you all",
-        "you'd":"you would",
-        "you'll":"you will",
-        "you're":"you are",
-        "you've":"you have",
-        "Whatcha":"What are you",
-        "luv":"love",
-        "sux":"sucks"
+        "pq":"porque",
+        "xq":"porque",
+        "tb":"tambien",
+        "fyi":"para tu atencion",
+        "k":"que",
+        "bro":"hermano",
+        "bb": "bebe"
         }
 
 
@@ -220,20 +104,20 @@ def tweet_cleaning_for_sentiment_analysis(tweet):
     tweet = tweet.replace('\x92',"'")
     
     #Removal of hastags/account
-    tweet = ' '.join(re.sub("(@[\s?_?A-Za-z0-9]+)|(#[\s?_?A-Za-z0-9]+)", " ", tweet).split())
+    tweet = ' '.join(re.sub("(@[\s?_?\wA-Za-z0-9]+)|(#[\s?_?\w]+)", "", tweet).split())
     
     #Removal of address
-    tweet = ' '.join(re.sub("(\w+:\/\/\S+)", " ", tweet).split())
+    tweet = ' '.join(re.sub("(\w+:\/\/\S+)", "", tweet).split())
     
     #Removal of Punctuation
-    tweet = ' '.join(re.sub("[\.\,\!\?\:\;\-\=\&]", " ", tweet).split())
+    tweet = ' '.join(re.sub("[\.\,\!\?\:\;\-\=\&\´\``\|\[\]\*\)\(\%\>\<\#\/]", "", tweet).split())
     
     #Lower case
     tweet = tweet.lower()
     
     #CONTRACTIONS source: https://en.wikipedia.org/wiki/Contraction_%28grammar%29
     CONTRACTIONS = load_dict_contractions()
-    tweet = tweet.replace("’","'")
+    # tweet = tweet.replace("’","'")
     words = tweet.split()
     reformed = [CONTRACTIONS[word] if word in CONTRACTIONS else word for word in words]
     tweet = " ".join(reformed)
@@ -276,23 +160,23 @@ def preprocess(input_file, output_file, keep=1):
     i=0
     with open(output_file, 'w') as csvoutfile:
         csv_writer = csv.writer(csvoutfile, delimiter=' ', lineterminator='\n')
-        with open(input_file, 'r', newline='') as csvinfile: #,encoding='latin1'
+        with open(input_file, 'r', newline='',encoding='utf8') as csvinfile: #,encoding='latin1'
             csv_reader = csv.reader(csvinfile, delimiter=',', quotechar='"')
             for row in csv_reader:
                 if row[4]!="MIXED" and row[4].upper() in ['POSITIVE','NEGATIVE','NEUTRAL'] and row[2]!='':
                     row_output = transform_instance(row)
-                    csv_writer.writerow(row_output )
+                    csv_writer.writerow(row_output)
                     # print(row_output)
                 i=i+1
                 if i%10000 ==0:
                     print(i)
             
 # Preparing the training dataset        
-preprocess('betsentiment-ES-tweets-sentiment-teams.csv', 'tweets.train')
+preprocess('prueba.csv', 'tweets.train')
 
 # Preparing the validation dataset        
 # preprocess('data.csv', 'tweets.validation')
-preprocess('betsentiment-ES-tweets-sentiment-teams.csv', 'tweets.validation')
+preprocess('prueba.csv', 'tweets.validation')
 
 
 
@@ -369,7 +253,7 @@ upsampling( 'tweets.train','uptweets.train')
 training_data_path ='uptweets.train' 
 validation_data_path ='tweets.validation'
 model_path =''
-model_name="model-en"
+model_name="model-es"
 
 def train():
     print('Training start')
